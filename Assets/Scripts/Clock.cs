c1396b199ef77f4c887dfd203ad292542c0b210e
orlogan <orlogan7@gmail.com>
2020-02-13 17:38:06 -0600

    First Commit

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Clock : MonoBehaviour {
+
+    public Transform hoursTransform, minutesTransform, secondsTransform;
+
+    private void Awake() {
+        hoursTransform.Rotate(0, DateTime.Now.Hour * 30, 0);
+        minutesTransform.Rotate(0, DateTime.Now.Minute * 6, 0);
+        secondsTransform.Rotate(0, DateTime.Now.Second * 6, 0);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hoursTransform.Rotate(0, Time.deltaTime/(60*60), 0);
+        minutesTransform.Rotate(0, Time.deltaTime / (60), 0);
+        secondsTransform.Rotate(0, Time.deltaTime, 0);
+    }
+}
---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour {
+
+    public Transform pointPrefab;
+    private Transform[] points;
+     [Range(10,100)]
+    public int resolution = 10;
+
+    private void Awake() {
+
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
+        Vector3 position;
+        position.y = 0;
+        position.z = 0;
+
+        points = new Transform[resolution];
+        for(int i = 0; i < points.Length; i++) {
+            points[i] = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            points[i].localPosition = position;
+            points[i].localScale = scale;
+            points[i].SetParent(transform,false);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < points.Length; i++) {
+            Transform point = points[i];
+            Vector3 position = points[i].localPosition;
+            position.y = Mathf.Exp(position.x);
+            point.localPosition = position;
+        }
+	}
+}
